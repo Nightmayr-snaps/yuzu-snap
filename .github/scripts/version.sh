be25f8729d0afa7a172b003ec9de015c41020f55
Nightmayr <umayr23@gmail.com>
2020-06-30 23:49:32 +0100

    added replacing version logic

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -2,17 +2,18 @@
 
 # check latest tagged version
 LATEST_VERSION_TAG="$(curl https://api.github.com/repos/yuzu-emu/yuzu-mainline/releases/latest -s | jq .tag_name -r)"
-CURRENT_VERSION_SNAP="$(grep version snap/snapcraft.yaml | cut -d ' ' -f2 | tr -d \"\'\")"
+export CURRENT_VERSION_SNAP="$(grep version snap/snapcraft.yaml | cut -d ' ' -f2 | tr -d \"\'\")"
 CURRENT_VERSION_TAG_SNAP=mainline-$CURRENT_VERSION_SNAP
-build=true
+export LATEST_VERSION=${LATEST_VERSION_TAG#mainline-}
+
 
 # compare versions
 if [ $CURRENT_VERSION_TAG_SNAP != $LATEST_VERSION_TAG ]; then
     echo "versions don't match, github: $LATEST_VERSION_TAG snap: $CURRENT_VERSION_TAG_SNAP"
-    # replace version in yaml
+    export BUILD=true
 else
     echo "versions match, github: $LATEST_VERSION_TAG snap: $CURRENT_VERSION_TAG_SNAP"
-    # terminate workflow
+    export BUILD=false
 fi
 
 
